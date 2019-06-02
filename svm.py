2c412c9116c04f293295253807f86225fc70b710
yullidias <yulli.dias@hotmail.com>
2019-06-02 10:55:50 -0300

    adicionando matrix de confusão

--- svm.py
+++ svm.py
@@ -1,12 +1,13 @@
 import preprocess
 from sklearn import svm
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
+
 
 vocabularioTreino, training, vocabularioTest, test = preprocess.preprocess()
 
-classifier = svm.SVC(kernel='poly')
+classifier = svm.SVC(kernel='poly', gamma='auto')
 classifier.fit(vocabularioTreino, training['Polaridade'])
 prediction = classifier.predict(vocabularioTest)
 
-report = classification_report(test['Polaridade'], prediction)#, output_dict=True)
-print(report)
+print(classification_report(test['Polaridade'], prediction)) #, output_dict=True))
+# print(confusion_matrix(test['Polaridade'], prediction))
