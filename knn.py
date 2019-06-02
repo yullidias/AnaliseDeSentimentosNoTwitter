087c0765bd084744df851d93fdc7ae1cde14e685
JuGP <julianegp@hotmail.com>
2019-06-02 07:28:28 -0300

    Update

--- knn.py
+++ knn.py
@@ -1,24 +1,20 @@
-import numpy as np  
-import matplotlib.pyplot as plt  
-import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier  
 from sklearn.metrics import classification_report, confusion_matrix
 
-import analise
+import preprocess
 
 #Preprocessing
-vocabulary, X_train, y_train, X_test, y_test = analise.inicia()
+X_train, y_train, X_test, y_test = preprocess.preprocess()
 
-print("Vocabulario: "+str(len(vocabulary)))
 print("Entradas Treino: "+str(len(y_train)))
 print("Entradas Teste: "+str(len(y_test)))
 print("Entradas Totais: "+str(len(y_train)+len(y_test)))
 
 #Training and Predictions 
-classifier = KNeighborsClassifier(n_neighbors=3,algorithm='kd_tree')  
-classifier.fit(X_train, y_train) 
+classifier = KNeighborsClassifier(n_neighbors=3)  
+classifier.fit(X_train, y_train['Polaridade']) 
 y_pred = classifier.predict(X_test)
 
 #Evaluating the Algorithm
-print(confusion_matrix(y_test, y_pred))  
-print(classification_report(y_test, y_pred)) 
+print(confusion_matrix(y_test['Polaridade'], y_pred))  
+print(classification_report(y_test['Polaridade'], y_pred)) 
