cac7e107644a5e5e75df8e39e50f125bcc1c2c3b
yullidias <yulli.dias@hotmail.com>
2019-06-02 21:32:42 -0300

    exibindo tamanho do treino de cada classe e usando kernel linear

--- svm.py
+++ svm.py
@@ -2,12 +2,14 @@ import preprocess
 from sklearn import svm
 from sklearn.metrics import classification_report, confusion_matrix
 
+vocabularioTreino, training, vocabularioTest, test = preprocess.preprocess(isToStemWords=True)
+print(len(training.loc[training['Polaridade'] == 1 ]))
+print(len(training.loc[training['Polaridade'] == -1 ]))
+print(len(training.loc[training['Polaridade'] == 0 ]))
 
-vocabularioTreino, training, vocabularioTest, test = preprocess.preprocess()
-
-classifier = svm.SVC(kernel='poly', gamma='auto')
+classifier = svm.SVC(kernel='linear')
 classifier.fit(vocabularioTreino, training['Polaridade'])
 prediction = classifier.predict(vocabularioTest)
 
 print(classification_report(test['Polaridade'], prediction)) #, output_dict=True))
-# print(confusion_matrix(test['Polaridade'], prediction))
+print(confusion_matrix(test['Polaridade'], prediction))
