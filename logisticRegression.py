55a01d218340ff7ccbfa5a609c9335a03dc1c58e
Sarah Rodrigues <sarahrodriguesmoreira@gmail.com>
2019-06-10 00:59:41 -0300

    Finalizando

--- logisticRegression.py
+++ logisticRegression.py
@@ -7,13 +7,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-myRange = [0.001,0.01,0.1,1,10]
+myRange =[0.00001,0.00005,0.0001,0.0005,0.001]
 
 def findBestC(): #encontrar o melhor valor para regularização#
     plt.title("Regressão Logística")
     plt.xlabel("Fator de Regularização")
     plt.ylabel("Acurácia")
-
 
     X_train, y_train, X_test, y_test = preprocess.preprocess(isToRemoveStopWords=False, isToStemWords=False)
     accuracity = []
@@ -23,7 +22,7 @@ def findBestC(): #encontrar o melhor valor para regularização#
         y_pred = classifier.predict(X_test)
         w = accuracy_score(y_pred,y_test['Polaridade'])
         accuracity.append(w)
-    plt.plot(myRange, accuracity, color='green', label='None')
+    plt.plot(myRange, accuracity, color='green', label='Nenhum')
 
     X_train, y_train, X_test, y_test = preprocess.preprocess(isToRemoveStopWords=False, isToStemWords=True)
     accuracity = []
@@ -33,7 +32,7 @@ def findBestC(): #encontrar o melhor valor para regularização#
         y_pred = classifier.predict(X_test)
         w = accuracy_score(y_pred,y_test['Polaridade'])
         accuracity.append(w)
-    plt.plot(myRange,accuracity,color='red', label='Stem')
+    plt.plot(myRange,accuracity,color='red', label='Stem Words')
 
     X_train, y_train, X_test, y_test = preprocess.preprocess(isToRemoveStopWords=True, isToStemWords=False)
     accuracity = []
@@ -53,17 +52,20 @@ def findBestC(): #encontrar o melhor valor para regularização#
         y_pred = classifier.predict(X_test)
         w = accuracy_score(y_pred,y_test['Polaridade'])
         accuracity.append(w)
-    plt.plot(myRange,accuracity,color='yellow', label='Both')
-
+    plt.plot(myRange,accuracity,color='yellow', label='Ambos')    
+    plt.legend(loc='lower right')
     plt.show()
 
 
 accuracity = []
-DF = [0.2,0.4,0.6,0.8]
+DF = [0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5]
 def findBestDF():
+    plt.title("Regressão Logística")
+    plt.xlabel("MIN_DF")
+    plt.ylabel("Acurácia")
     for x in DF:
-        X_train, y_train, X_test, y_test = preprocess.preprocess(isToRemoveStopWords=True, isToStemWords=False, min_df=x)
-        classifier = OneVsRestClassifier(KNeighborsClassifier(n_neighbors=4))
+        X_train, y_train, X_test, y_test = preprocess.preprocess(isToRemoveStopWords=False, isToStemWords=False, min_df=x)
+        classifier = OneVsRestClassifier(LogisticRegression(solver='liblinear',C=0.00001))
         classifier.fit(X_train, y_train['Polaridade']) 
         y_pred = classifier.predict(X_test)
         w = accuracy_score(y_pred,y_test['Polaridade'])
@@ -73,21 +75,9 @@ def findBestDF():
     plt.show()
 
 
-
-findBestC()
-findBestDF()
-
-'''
-X_train, X_test, y_train, y_test = preprocess.preprocess(isToRemoveStopWords=True, isToStemWords=False,min_df=0.2)
-
-
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-
-
-classifier = OneVsRestClassifier(KNeighborsClassifier(n_neighbors=4))
+X_train, y_train, X_test, y_test = preprocess.preprocess(isToRemoveStopWords=False, isToStemWords=False, min_df=0.2)
+classifier = OneVsRestClassifier(LogisticRegression(solver='liblinear',C=0.00001))
 classifier.fit(X_train, y_train['Polaridade']) 
 y_pred = classifier.predict(X_test)
 print(confusion_matrix(y_test['Polaridade'], y_pred))  
 print(classification_report(y_test['Polaridade'], y_pred))
-'''
